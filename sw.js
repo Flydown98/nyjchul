73c105a5582c25909d1d7d9b26530eb82cdb5dd2
Flydown98 <qazw1385@gmail.com>
2026-06-28 23:22:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nyj-sheets-calendar-final-v6';
+const CACHE='nyj-sheets-calendar-config-fixed-v7';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./install.html','./install.js','./assets/icon-192.png','./assets/icon-512.png','./assets/signature_horizontal_color.png','./assets/symbol_color.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
