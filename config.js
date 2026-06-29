27d86af1fc37089195aee767083173773ed549e7
Flydown98 <qazw1385@gmail.com>
2026-06-29 11:17:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// 남양주시장애인복지관 일정·출석·리뷰 웹앱 설정 파일
-// 중요: app.js에서 window.APP_CONFIG로 읽기 때문에 반드시 window.APP_CONFIG로 선언해야 합니다.
 window.APP_CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzAVaUmkWsWSR_R0GmL_ijyLsie0bw7fIuyR8wuo_cZsDQVxeF7PrwXbXkgaUmt4g0u/exec",
   LOCAL_SAMPLE_WHEN_EMPTY: false,
